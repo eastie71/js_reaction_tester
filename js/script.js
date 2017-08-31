0cc55f3785df9a89d0bf8bfff1e2a170705ec7b3
Craig Eastwood <craigdavideastwood@gmail.com>
2017-08-31 17:59:31 +1000

    Initial files from course

---
+++ js/script.js
@@ -0,0 +1,73 @@
+		createShape();
+		var startTime = new Date().getTime();
+	
+		function setShapeType() {
+			var shapeType = (Math.floor(Math.random()*2));
+
+			if (shapeType == 0) {
+				document.getElementById('coloredShape').style.borderRadius = "0%";
+			} else {
+				document.getElementById('coloredShape').style.borderRadius = "50%";
+			}
+			return;
+		}
+
+		function setShapeSize() {
+			var shapeSize = (Math.floor(Math.random()*300)+30);
+			document.getElementById('coloredShape').style.width = shapeSize + "px";
+			document.getElementById('coloredShape').style.height = shapeSize + "px";
+			return;
+		}
+
+		function setShapePos() {
+			var maxTopMargin = window.screen.availHeight - 400;
+			var maxLeftMargin = window.screen.availWidth - 200;
+
+			var marginTop = (Math.floor(Math.random()*maxTopMargin));
+			var marginLeft = (Math.floor(Math.random()*maxLeftMargin));
+			document.getElementById('coloredShape').style.marginTop = marginTop + "px";
+			document.getElementById('coloredShape').style.marginLeft = marginLeft + "px";
+			return;
+		}
+
+		function setShapeColor() {			
+			var colorNum = (Math.floor(Math.random()*16777215));
+			var hexColorNum = decimalToHex(colorNum, 2);
+
+			document.getElementById('coloredShape').style.backgroundColor = "#" + hexColorNum;
+		}
+
+		function decimalToHex(d, padding) {
+			var hex = Number(d).toString(16);
+			padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
+			while (hex.length < padding) {
+        		hex = "0" + hex;
+    		}
+    		return hex;
+		}
+
+		function createShape() {
+			setShapeSize();
+			setShapeType();
+			setShapePos();
+			setShapeColor();
+			document.getElementById('coloredShape').style.display = "block";
+		}
+
+		document.getElementById('coloredShape').onclick = function () {
+			// body...
+			var endTime = new Date().getTime();
+			var timeTaken = (endTime - startTime)/1000;
+			document.getElementById('timeResult').innerHTML = "Your time: " + timeTaken + "s";
+
+			var delayTime = (Math.random()*1500);
+
+			document.getElementById('coloredShape').style.display = "none";
+
+			setTimeout(createShape, delayTime);
+			
+			startTime = new Date().getTime();
+
+			//document.getElementById('coloredShape').style.display = "none";
+
+		}
